df3b50e7c1be005b921da56491710fcb23d8dfc9
Fabio <fats140701@gmail.com>
2023-02-13 14:45:38 -0400

    parcial refactor

--- src/controllers/enrollments-controller.ts
+++ src/controllers/enrollments-controller.ts
@@ -1,42 +1,55 @@
-import { AuthenticatedRequest } from "@/middlewares";
-import enrollmentsService from "@/services/enrollments-service";
-import { Response } from "express";
+import { Request, Response } from "express";
 import httpStatus from "http-status";
+import enrollmentsService from "@/services/enrollments-service";
 
-export async function getEnrollmentByUser(req: AuthenticatedRequest, res: Response) {
+export async function getEnrollmentByUser(
+  req: Request,
+  res: Response
+): Promise<void> {
   const { userId } = req;
-
   try {
-    const enrollmentWithAddress = await enrollmentsService.getOneWithAddressByUserId(userId);
-
-    return res.status(httpStatus.OK).send(enrollmentWithAddress);
+    const enrollmentWithAddress =
+      await enrollmentsService.getOneWithAddressByUserId(userId);
+    res.status(httpStatus.OK).send(enrollmentWithAddress);
   } catch (error) {
-    return res.sendStatus(httpStatus.NO_CONTENT);
+    res.sendStatus(httpStatus.NO_CONTENT);
   }
 }
 
-export async function postCreateOrUpdateEnrollment(req: AuthenticatedRequest, res: Response) {
+export async function postCreateOrUpdateEnrollment(
+  req: Request,
+  res: Response
+): Promise<void> {
+  const { body } = req;
   try {
     await enrollmentsService.createOrUpdateEnrollmentWithAddress({
-      ...req.body,
+      ...body,
       userId: req.userId,
     });
-
-    return res.sendStatus(httpStatus.OK);
+    res.sendStatus(httpStatus.OK);
   } catch (error) {
-    return res.sendStatus(httpStatus.BAD_REQUEST);
+    res.sendStatus(httpStatus.BAD_REQUEST);
   }
 }
 
-export async function getAddressFromCEP(req: AuthenticatedRequest, res: Response) {
-  const { cep } = req.query as Record<string, string>;
+export async function getAddressFromCEP(
+  req: Request,
+  res: Response
+): Promise<void> {
+  const cep = req.query?.cep as string;
+  if (!cep) {
+    res.sendStatus(httpStatus.BAD_REQUEST);
+    return;
+  }
 
   try {
     const address = await enrollmentsService.getAddressFromCEP(cep);
-    return res.status(httpStatus.OK).send(address);
+    res.status(httpStatus.OK).send(address);
   } catch (error) {
     if (error.name === "NotFoundError") {
-      return res.sendStatus(httpStatus.NO_CONTENT);
+      res.sendStatus(httpStatus.NO_CONTENT);
+      return;
     }
+    res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
   }
 }
--- src/controllers/payments-controller.ts
+++ src/controllers/payments-controller.ts
@@ -1,42 +1,50 @@
-import { AuthenticatedRequest } from "@/middlewares";
-import paymentService from "@/services/payments-service";
-import { Response } from "express";
+import { Request, Response } from "express";
 import httpStatus from "http-status";
 
-export async function getPaymentByTicketId(req: AuthenticatedRequest, res: Response) {
-  try {
-    const ticketId = Number(req.query.ticketId);
-    const { userId } = req;
+import { AuthenticatedRequest } from "@/middlewares";
+import paymentService from "@/services/payments-service";
 
-    if (!ticketId) {
-      return res.sendStatus(httpStatus.BAD_REQUEST);
-    }
+export async function getPaymentByTicketId(
+  req: AuthenticatedRequest,
+  res: Response
+) {
+  const { userId } = req;
+  const ticketId = Number(req.query.ticketId);
+
+  if (!ticketId) {
+    return res.sendStatus(httpStatus.BAD_REQUEST);
+  }
+
+  try {
     const payment = await paymentService.getPaymentByTicketId(userId, ticketId);
 
     if (!payment) {
       return res.sendStatus(httpStatus.NOT_FOUND);
     }
+
     return res.status(httpStatus.OK).send(payment);
   } catch (error) {
     if (error.name === "UnauthorizedError") {
       return res.sendStatus(httpStatus.UNAUTHORIZED);
     }
-    return res.sendStatus(httpStatus.NOT_FOUND);
+    return res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
   }
 }
 
 export async function paymentProcess(req: AuthenticatedRequest, res: Response) {
-  try {
-    const { userId } = req;
-    const {
-      ticketId,
-      cardData,
-    } = req.body;
+  const { userId } = req;
+  const { ticketId, cardData } = req.body;
 
-    if (!ticketId || !cardData) {
-      return res.sendStatus(httpStatus.BAD_REQUEST);
-    }
-    const payment = await paymentService.paymentProcess(ticketId, userId, cardData);
+  if (!ticketId || !cardData) {
+    return res.sendStatus(httpStatus.BAD_REQUEST);
+  }
+
+  try {
+    const payment = await paymentService.paymentProcess(
+      ticketId,
+      userId,
+      cardData
+    );
 
     if (!payment) {
       return res.sendStatus(httpStatus.NOT_FOUND);
@@ -47,6 +55,6 @@ export async function paymentProcess(req: AuthenticatedRequest, res: Response) {
     if (error.name === "UnauthorizedError") {
       return res.sendStatus(httpStatus.UNAUTHORIZED);
     }
-    return res.sendStatus(httpStatus.NOT_FOUND);
+    return res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
   }
 }
